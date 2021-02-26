7d9b931e7c9d4b62a07f9f138b410627eda997e8
Woong Kwon <wkwon@stu.jsu.edu>
2021-02-25 22:29:28 -0600

    Creat Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
 function App() {
+
+  const customers = [{
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': 'Connor',
+    'birthday': '950322',
+    'gender': 'Male',
+    'job': 'Student'    
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': 'David',
+    'birthday': '950123',
+    'gender': 'Male',
+    'job': 'Software Developer'    
+  },
+  { 'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': 'Anna',
+    'birthday': '970230',
+    'gender': 'Female',
+    'job': 'Nurse'
+  }
+] 
+
   return (
-    <div className = "gray-background">
-      <img src={logo} lat = "logo"/>
-      <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return(
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })
+      }
     </div>
   );
 }
