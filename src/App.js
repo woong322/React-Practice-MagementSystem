ce07a8029ae8fa091b327f851a74e7e7b87966c1
Woong Kwon <wkwon@stu.jsu.edu>
2021-02-25 23:26:54 -0600

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
 
-function App() {
+  const styles = theme => ({
+    root: {
+      width: '100%',
+      marginTop: theme.spacing.unit * 3,
+      overflowX: "auto"
+    },
+    table: {
+      minWidth: 1080
+    }
+  })
 
   const customers = [{
     'id': 1,
@@ -27,27 +43,42 @@ function App() {
     'gender': 'Female',
     'job': 'Nurse'
   }
-] 
-
-  return (
-    <div>
-      {
-        customers.map(c => {
-          return(
-            <Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-          );
-        })
-      }
-    </div>
-  );
+  ] 
+  class App extends Component {
+    render() {
+      const { classes } =this.props;
+      return (
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                  <TableCell>Number</TableCell>
+                  <TableCell>Picture</TableCell>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Birthday</TableCell>
+                  <TableCell>Gender</TableCell>
+                  <TableCell>Job</TableCell>
+              </TableRow>
+            </TableHead>
+          <TableBody>
+          {customers.map(c => {
+              return(
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              );
+            })
+          }
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
-
-export default App;
+  export default withStyles(styles)(App);
